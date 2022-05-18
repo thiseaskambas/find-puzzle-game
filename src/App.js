aff5211ad5890ed2d84d88c0a11dfc957cc86fef
thiseaskambas <tkambas@yahoo.com>
2022-05-18 17:12:58 -0400

    getting correct coordinates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { useState } from "react";
+import puzzleImg from "./assets/puzzle.jpeg";
+import styles from "./App.module.css";
 function App() {
-  return <div></div>;
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  function clickHandler(e) {
+    const { width, height } = e.target.getBoundingClientRect();
+    const { offsetX, offsetY } = e.nativeEvent;
+
+    setX(Math.round((offsetX / width) * 100));
+    setY(Math.round((offsetY / height) * 100));
+    console.log({ x, y });
+  }
+
+  return (
+    <div className={styles.imgCtn}>
+      <img src={puzzleImg} onClick={clickHandler} />
+    </div>
+  );
 }
 
 export default App;
